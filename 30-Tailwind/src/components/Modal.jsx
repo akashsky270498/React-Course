147965ac20b7c0237d2fc45464affcca7bdcb4f1
Akash <akashsky270498@gmail.com>
2025-07-07 20:18:19 +0530

    fixed the css

--- 30-Tailwind/src/components/Modal.jsx
+++ 30-Tailwind/src/components/Modal.jsx
@@ -1,17 +1,19 @@
 import { createPortal } from "react-dom";
 
 const Modal = ({ isOpen, setIsOpen, header, footer, children }) => {
+  if (!isOpen) return null;
+
   return createPortal(
     <div
       onClick={() => setIsOpen(false)}
-      className={`fixed inset-0 flex items-center justify-center bg-black/40 px-4 ${isOpen ? "" : "hidden"}`}
+      className={`fixed inset-0 z-50 flex items-center justify-center bg-black/40 px-4 ${isOpen ? "" : "hidden"}`}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="max-w-2xl grow rounded-lg bg-white p-4 shadow-lg"
+        className="w-full max-w-md grow rounded-2xl bg-white p-6 shadow-xl transition-all"
       >
         {header}
-        <div className="-mx-4 my-3 flex flex-wrap gap-4 border-y px-4 py-4">
+        <div className="mx-4 my-3 flex flex-wrap gap-4 border-y px-4 py-4">
           {children}
         </div>
         {footer}
